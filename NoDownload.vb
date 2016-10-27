e05b3e73762a1b535c093f1874e412c94b7c3a20
justalittlekid <land.man@gmx.de>
2016-10-27 20:57:39 +0200

    NoDownload.vb
    If you dont want to download the pics, use this code.

---
+++ NoDownload.vb
@@ -0,0 +1,27 @@
+Imports System.Text
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Based on https://github.com/indrajeetb 
+        Dim chars2use As String = "abcdef"
+
+        Dim sb As New StringBuilder()
+        Dim rand1 As New Random()
+        For i As Integer = 1 To 1
+            Dim idx As Integer = rand1.Next(0, chars2use.Length)
+            Dim randomChar As Char = chars2use(idx)
+            sb.Append(randomChar)
+
+            Dim rand As New Random()
+            Dim number = rand.Next(1111, 9999) & randomChar & ".png"
+
+            Process.Start("http://l.omegle.com/" & number)
+
+
+        Next i
+
+
+        Dim randomString = sb.ToString()
+    End Sub
+
+End Class
